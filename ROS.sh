cc5541b2ab998fa9a951bd3985c56b85a2748988
CR351 <22698761+CR351@users.noreply.github.com>
2018-05-23 11:24:25 -0400

    Update ROS.sh

--- ROS.sh
+++ ROS.sh
@@ -6,7 +6,7 @@
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list' \
     && apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 
-sudo apt-get update && install -y \
+sudo apt-get update && apt-get install -y \
     ros-melodic-desktop-full
     
 # To find available packages, use:
