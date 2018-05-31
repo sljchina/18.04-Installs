2fdec9af42de2f3cb398d8977488c7d7e5a8c9e2
CR351 <22698761+CR351@users.noreply.github.com>
2018-05-31 08:55:58 -0400

    Update Cudnn

--- CUDA.sh
+++ CUDA.sh
@@ -1,52 +1,30 @@
 #!/bin/bash 
 
-# Check if Nvidia Drivers are installed
-nvidia-smi
+NVIDIA_GPGKEY_SUM=d1be581509378368edeec8c1eb2958702feedf3bc3d17011adbf24efacce4ab5 && \
+NVIDIA_GPGKEY_FPR=ae09fe4bbd223a84b2ccfce3f60f4b3d7fa2af80 && \
+apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub && \
+apt-key adv --export --no-emit-version -a $NVIDIA_GPGKEY_FPR | tail -n +5 > cudasign.pub && \
+echo "$NVIDIA_GPGKEY_SUM  cudasign.pub" | sha256sum -c --strict - && rm cudasign.pub && \
+echo "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64 /" > /etc/apt/sources.list.d/cuda.list && \
+echo "deb https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64 /" > /etc/apt/sources.list.d/nvidia-ml.list
 
-echo -e \
-    " Is no driver are found install Nvidia Drivers \
-    \
-    sudo apt purge nvidia-* \
-    && add-apt-repository ppa:graphics-drivers/ppa \
-    && apt update \
-    && apt install nvidia-390 \"
-    
-read
 
-# Install wget
-sudo apt-get update && install -y \
-    wget
-   
-# Download Cuda-9.0
-cd ~/Downloads   
-wget https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda_9.0.176_384.81_linux-run
-
-# Install Cuda-9.0
-echo -e \
-    "Select the folowwing: \
-    Yes to installing with an unsupported configuration, \
-    No to “Install NVIDIA Accelerated Graphics Driver for Linux-x86_64 384.81?”. \n
-    Don’t agree to install the new driver"
-
-sudo chmod +x cuda_9.0.176_384.81_linux.run
-
-./cuda_9.0.176_384.81_linux.run --override
-
-# Install Cudnn-7.0 (CUDA® Deep Neural Network)
-
-CUDNN_VERSION = 7.1.4.18
+CUDNN_VERSION=7.1.4.18
 
 sudo apt-get install -y --no-install-recommends \
     libcudnn7=$CUDNN_VERSION-1+cuda9.0 \
     libcudnn7-dev=$CUDNN_VERSION-1+cuda9.0
- 
+    
+
 # Install CUPTI (CUDA® Profiling Tools Interface)
 sudo apt-get install cuda-command-line-tools
+
 
 # Environment setup
 export PATH=/usr/local/cuda-9.0/bin${PATH:+:${PATH}}
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:+${LD_LIBRARY_PATH}:}/usr/local/cuda/extras/CUPTI/lib64
+
 
 # Install NVIDIA TensorRT 3.0 (optimized inferencing performance)
 wget https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1404/x86_64/nvinfer-runtime-trt-repo-ubuntu1404-3.0.4-ga-cuda9.0_1.0-1_amd64.deb
@@ -54,6 +32,4 @@ sudo dpkg -i nvinfer-runtime-trt-repo-ubuntu1404-3.0.4-ga-cuda9.0_1.0-1_amd64.de
 sudo apt-get update && apt-get install -y \
     libnvinfer-dev
 
-# Install TensorFlow with GPU support
-pip3 install --upgrade tensorflow-gpu
 
