6ba22003b153951f0b7278d9eed3f7d790a3056d
CR351 <22698761+CR351@users.noreply.github.com>
2018-06-14 14:40:22 -0400

    Update CUDA.sh

--- CUDA.sh
+++ CUDA.sh
@@ -32,18 +32,21 @@ sudo chmod +x cuda_9.0.176_384.81_linux.run
 
 ./cuda_9.0.176_384.81_linux.run --override
 
-# Install Cudnn-7.0 (CUDA® Deep Neural Network)
+# Install Cudnn-7.0 (CUDA® Deep Neural Network) & Stop it from Updating
 
 CUDNN_VERSION = 7.1.4.18
 
-sudo apt-get install -y --no-install-recommends \
+sudo apt-get install -y --no-install-recommends --allow-downgrades \
+    libcudnn7=$CUDNN_VERSION-1+cuda9.0 \
+    libcudnn7-dev=$CUDNN_VERSION-1+cuda9.0 \
+    && apt-mark hold \
     libcudnn7=$CUDNN_VERSION-1+cuda9.0 \
     libcudnn7-dev=$CUDNN_VERSION-1+cuda9.0
  
 # Install CUPTI (CUDA® Profiling Tools Interface)
 sudo apt-get install libcupti-dev
 
-# Environment setup
+# Environment setup (If you have to run more than once go and clean up ~/.profile)
 export PATH=/usr/local/cuda-9.0/bin${PATH:+:${PATH}}
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 
